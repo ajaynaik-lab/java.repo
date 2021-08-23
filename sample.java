3c9a97f61546f316ecf02603fbd1bea57e2f7ad9
ajaynaik-lab <89064556+ajaynaik-lab@users.noreply.github.com>
2021-08-23 04:59:53 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 class Simple{
 public static void main(String args[]) throws InterruptedException{
 Thread.sleep(10000);
-System.out.println("Hello Java updated for branch b1");
+System.out.println("Hello Java updated");
 }
 }
